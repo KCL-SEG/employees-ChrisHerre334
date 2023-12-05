2151993fbbd07a69ba990dcab9c4fbc482ed7204
ChrisHerre334 <chrisherre2003@gmail.com>
2023-12-05 07:09:26 +0000

    Move fullstops

--- employee.py
+++ employee.py
@@ -33,20 +33,20 @@ class Employee:
         pay_description = f'{self.name} works on '
 
         if self.contract_type == 'salary':
-            pay_description += f'a monthly salary of {self.salary}.'
+            pay_description += f'a monthly salary of {self.salary}'
         elif self.contract_type == 'hourly':
-            pay_description += f'a contract of {self.hours_worked} hours at {self.hourly_wage}/hour.'
+            pay_description += f'a contract of {self.hours_worked} hours at {self.hourly_wage}/hour'
         else:
             pay_description += f'an unknown contract type.'
 
         total_pay = self.get_pay()
 
         if self.contracts_landed and self.commission_per_contract:
-            pay_description += f' and receives a commission for {self.contracts_landed} contract(s) at {self.commission_per_contract}/contract.'
+            pay_description += f' and receives a commission for {self.contracts_landed} contract(s) at {self.commission_per_contract}/contract'
         elif self.bonus:
-            pay_description += f' and receives a bonus commission of {self.bonus}.'
+            pay_description += f' and receives a bonus commission of {self.bonus}'
 
-        pay_description += f' Their total pay is {total_pay}.'
+        pay_description += f'. Their total pay is {total_pay}.'
 
         return pay_description
 
